a61d9e9e28ca5e54965f758e9f986d832d6567e8
Desmond Mullen <dsm@busylittlestudios.com>
2019-03-20 23:44:41 -0400

    working

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,18 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
-require('dotenv').config();
+const connection;
 
-const DB_HOST = process.env.DB_HOST || 'localhost';
-const DB_USERNAME = process.env.DB_USERNAME || 'root';
-const DB_PASSWORD = process.env.DB_PASSWORD || 'vivian';
-const DB_DATABASE = process.env.DB_DATABASE || 'burgers_db'
-
-const connection = mysql.createConnection({
-    host: DB_HOST,
-    port: 3306,
-    user: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_DATABASE
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'vivian',
+        database: 'burgers_db'
+    });
+};
 
 // Make connection.
 connection.connect(err => {
